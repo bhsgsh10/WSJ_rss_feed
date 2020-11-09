a9d4583a8e54b0c0b0c4df100e0122e9add916c1
Bhaskar <bhsgsh@gmail.com>
2020-11-09 18:33:40 -0500

    Initial Commit

---
+++ WSJFeed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WSJFeed
+//
+//  Created by Bhaskar Ghosh on 11/9/20.
+//  Copyright © 2020 Bhaskar Ghosh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
